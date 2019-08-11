d8f0ee790ae665d3eecd59dcda1a691f83033d0a
Glitch (capable-pruner) <none>
2019-08-11 19:53:23 +0000

    🙇🚲 Checkpoint ./bot.js:5138149/4168

--- bot.js
+++ bot.js
@@ -419,7 +419,25 @@ let kanalid = `610192034543697960`;
 //----------------------------------Destek Sistemi SON-----------------------------// 
 
 
+client.on('ready', async() => {
 
+let server;
+
+ setInterval(() => {
+ if(client.channels.has('592430602419044365')) server = client.channels.get('592430605300662272').guild.id
+
+
+var query = require('game-server-query');
+  query(
+    {
+        type: 'csgo',
+        host: "185.193.165.189"
+    })
+ client.channels.find("id",'592430595947102250').setName('Adı : ' + query.name + '1');
+ client.channels.find("id",'592430598040059944').setName('Map : ' + query.map);
+ client.channels.find("id",'592430600200388629').setName('Oyuncular : ' + query.players );
+  }, 6000); 
+});
   
 
 
