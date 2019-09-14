107905fa188a998e6f141048c0716b18f7e72e04
Glitch (silk-wrinkle) <none>
2019-09-14 12:05:01 +0000

    🍎👭 Checkpoint ./bot.js:3528245/1513 ./komutlar/guvemlik.js:3528245/1140

--- bot.js
+++ bot.js
@@ -469,11 +469,13 @@ query(
          
         }
         else {
+          /*
           console.log(state);
   client.channels.find("id",'612192244568555520').setName(`${state.name}`);
 client.channels.find("id",'612192315066548235').setName(`Map: ${state.map}`);  
 client.channels.find("id",'612192410327449639').setName(`Oyuncular: ${state.raw.numplayers}/${state.maxplayers}`); 
   client.channels.find("id",'612192430506115082').setName(`${state.query.host}`); 
+  */
   }
     }
 );
@@ -500,12 +502,12 @@ client.on('ready', async() => {
                 if(body.players.now) {
                   var s = body.duration
         const duration = moment.duration(s).format(" D [gün], H [saat], m [dakika], s [saniye]");
-      *
+      /*
 client.channels.find("id",'612193568085377024').setName(body.motd);
    client.channels.find("id",'612193588305985536').setName( `Aktif oyunucu: ${body.players.now}/${body.players.max}`);
    client.channels.find("id",'612278774666493953').setName( `${duration}'dır aktif.`);
  client.channels.find("id",'612193605746032640').setName(mcIP);
-//
+*/
                 } else {
                   
                    }
@@ -514,5 +516,38 @@ client.channels.find("id",'612193568085377024').setName(body.motd);
   });
 
 
+client.on('guildMemberAdd',async member => {
+  let user = client.users.get(member.id);
+  let chan = client.channels.get(db.fetch(`guvenlik${member.guild.id}`)) 
+       const Canvas = require('canvas')
+       const canvas = Canvas.createCanvas(360,100);
+       const ctx = canvas.getContext('2d');
+  
+  const resim1 = await Canvas.loadImage('https://cdn.discordapp.com/attachments/591299755976425493/614151181752860672/yhosgeldirrn.png')
+    const resim2 = await Canvas.loadImage('https://cdn.discordapp.com/attachments/591299755976425493/614164419768877056/yhosgeldirrn.png')
+    const kurulus = new Date().getTime() - user.createdAt.getTime();
+    const gün = moment.duration(kurulus).format("D")   
+    var kontrol;
+      if (kurulus > 2629800000) kontrol = resim2
+    if (kurulus < 2629800000) kontrol = resim1
 
+       const background = await Canvas.loadImage('https://cdn.discordapp.com/attachments/591299755976425493/614164413318168606/Adsz.png');
+       ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+   
+
+
+  const avatar = await Canvas.loadImage(member.user.displayAvatarURL);
+  ctx.drawImage(kontrol,0,0,canvas.width, canvas.height)
+  ctx.beginPath();
+    ctx.lineWidth = 4;
+  ctx.fill()
+    ctx.lineWidth = 4;
+  ctx.arc(180, 46, 36, 0, 2 * Math.PI);
+    ctx.clip();
+  ctx.drawImage(avatar, 143,10, 73, 72  );
+
+   
+       const attachment = new Discord.Attachment(canvas.toBuffer(), 'STARKs-güvenlik.png');
+    chan.send(attachment)
+});
   client.login(ayarlar.token)
