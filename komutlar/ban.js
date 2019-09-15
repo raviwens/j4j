a2ae4e0877907279c52d8d767d17c14d4e27a83e
Glitch (silk-wrinkle) <none>
2019-09-15 07:48:28 +0000

    🕍🕰 Checkpoint ./komutlar/ban.js:3528245/12929 ./emojiler.json:3528245/37 ./komutlar/emojibul.js:3528245/4045

--- komutlar/ban.js
+++ komutlar/ban.js
@@ -1,44 +1,51 @@
+
+    
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const db = require('quick.db');
 
-exports.run = (client, message, args) => {
-  if (!message.guild) {
-  const ozelmesajuyari = new Discord.RichEmbed()
-  .setColor(0xFF0000)
-  .setTimestamp()
-  .setAuthor(message.author.username, message.author.avatarURL)
-  .addField(':warning: Uyarı :warning:', '`ban` adlı komutu özel mesajlarda kullanamazsın.')
-  return message.author.sendEmbed(ozelmesajuyari); }
-  let guild = message.guild
-  let reason = args.slice(1).join(' ');
-  let user = message.mentions.users.first();
-  let modlog = guild.channels.find('name', 'mod-log');
-  if (!modlog) return message.reply('`mod-log` kanalını bulamıyorum.');
-  if (reason.length < 1) return message.reply('Ban sebebini yazmalısın.');
-  if (message.mentions.users.size < 1) return message.reply('Kimi banlayacağını yazmalısın.').catch(console.error);
+exports.run = async (bot, message, args) => {
+    if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send(":no_entry: Bu komudu kullanabilmek için `Üyeleri Yasakla` yetkisine sahip olmanız gerek.");
+    let reason = args.slice(1).join(' ')
+    if (!args[0]) return message.channel.send(":no_entry: Yasaklamak istediğiniz kullanıcıyı etiketleyiniz.")
+    let user = message.mentions.users.first() || bot.users.get(args[0]) || message.guild.members.find(u => u.user.username.toLowerCase().includes(args[0].toLowerCase())).user
 
-  if (!message.guild.member(user).bannable) return message.reply('Yetkilileri banlayamam.');
-  message.guild.ban(user, 2);
-
-  const embed = new Discord.RichEmbed()
-    .setColor(0x00AE86)
-    .setTimestamp()
-    .addField('Eylem:', 'Ban')
-    .addField('Kullanıcı:', `${user.username}#${user.discriminator} (${user.id})`)
-    .addField('Yetkili:', `${message.author.username}#${message.author.discriminator}`)
-    .addField('Sebep', reason);
-  return guild.channels.get(modlog.id).sendEmbed(embed);
-};
+    if (!user) return message.channel.send(`${process.env.basarisiz} Etiketlediğin kullanıcıyı sunucuda bulamadım.`)
+    let member = message.guild.member(user)
+    if (!member) return message.channel.send(`${process.env.basarisiz} Etiketlediğin kullanıcıyı sunucuda bulamadım.`)
+    if (member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(`${process.env.basarisiz} Kendi yetkimin üstündeki kişileri yasaklayamam.`)
+    if (!reason) reason = 'Neden belirtilmemiş.'
+  
+    message.channel.send(`${user.tag}, adlı kullanıcıyı sunucudan yasaklayacağım emin misiniz? Eminseniz \`e\` işlemi iptal etmek ise \`h\` olarak cevaplayınız.`)
+        let uwu = false;
+            while (!uwu) {
+                const response = await message.channel.awaitMessages(neblm => neblm.author.id === message.author.id, { max: 1, time: 30000 });
+                const choice = response.first().content
+                if (choice == 'hayır' || choice == 'h') return message.channel.send('🚀 İşlem iptal **edildi.**')
+                if (choice !== 'evet' && choice !== 'e') {
+                message.channel.send('Lütfen sadece ``evet (e)`` veya ``hayır (h)`` ile cevap verin.')
+                }
+                if (choice == 'evet' || choice == 'e') uwu = true
+                }
+                if (uwu) {
+                try {
+                await member.ban(reason + ` | Yetkili: ${message.author.tag} - ${message.author.id}`)
+  
+                message.channel.send(`${process.env.basarili} **${user.tag}** başarıyla banlandı.`)
+            } catch(e) {
+            message.channel.send(':warning: Bir hata var!')
+        }
+    } else return console.log('Hata var')
+}
 
 exports.conf = {
   enabled: true,
   guildOnly: true,
   aliases: [],
-  permLevel: 3
+  permLevel: 0
 };
 
 exports.help = {
   name: 'ban',
-  description: 'İstediğiniz kişiyi banlar.',
-  usage: 'ban [kullanıcı] [sebep]'
+  description: 'nblm',
+  usage: 'ban'
 };
