5cfcd322b6e58b59f7749e56325751ac09ba88a1
Glitch (silk-wrinkle) <none>
2019-09-15 15:59:01 +0000

    🍠⛷ Checkpoint ./komutlar/afk.js:3528245/138 ./bot.js:3528245/103

--- bot.js
+++ bot.js
@@ -285,11 +285,11 @@ logKanal: "622708662087188481"//Log kanalı
         if(db.has(`afk_${afkadam.id}`)) {
           msg.delete(1000);
           const afkuyarı = new Discord.RichEmbed()
-              .setDescription(emojiler.gold1 + ` **${client.users.get(afkadam.id).username}** adlı kullanıcımız şuanda AFK \n\n**Sebep:** \n${db.fetch(`afksebep_${afkadam.id}`)}`)
+              .setDescription(emojiler.gold1 + ` **${client.users.get(afkadam.id).username}** adlı kullanıcımız şuanda AFK \n\n\`${db.fetch(`afksebep_${afkadam.id}`)}\` sebebi ile **AFK**`)
               .setColor('0xff6161')
               .setTimestamp()
           .setFooter(client.user.username + " AFK Sistemi", client.user.avatarURL)
-          msg.channel.send(afkuyarı).then(msg => msg.delete(5000));
+          msg.channel.send(afkuyarı)
         
           
         }
