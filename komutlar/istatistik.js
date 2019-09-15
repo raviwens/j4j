7fe49c21d428ef32f0923520293bc20e6a0b307c
Glitch (silk-wrinkle) <none>
2019-09-15 09:47:30 +0000

    🚋📖 Checkpoint ./komutlar/istatistik.js:3528245/4468 ./spam.js:3528245/214

--- komutlar/istatistik.js
+++ komutlar/istatistik.js
@@ -16,26 +16,18 @@ exports.run = (bot, message, args) => {
         }
         const duration = moment.duration(bot.uptime).format(" D [gün], H [saat], m [dakika], s [saniye]");
         const botBilgi = new Discord.RichEmbed()
-            .setAuthor(bot.user.username + " | İstatistikler", bot.user.avatarURL)
+            .setAuthor(bot.user.username + " - İstatistikler", bot.user.avatarURL)
             .setColor("0xc1ff05")
             .addField("❯ Bellek Kullanımı", `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} / ${(os.totalmem() / 1024 / 1024).toFixed(2)} MB`)
             .addField("❯ Çalışma Süresi ", `${duration}`)
-            .addField("❯ ", stripIndents
-            `\`\`\`yaml
-
-            Kullanıcı: ${bot.users.array().length}
-            Sunucu: ${bot.guilds.size.toLocaleString()} 
-            Kanal: ${bot.channels.size.toLocaleString()}
-            Ping: ${Math.round(bot.ping)}ms
-            \`\`\`
-            `)
-            .addField("❯ Versiyonlar", stripIndents`
-            » Discord.js: v${version}
-            » Node.js: ${process.version}
-            `)
+            .addField("❯ Sunucu",`${bot.guilds.size.toLocaleString()} `)
+           .addField("❯ Kanal",`${bot.channels.size.toLocaleString()}`)
+           .addField("❯ Kullanıcı", `${bot.users.array().length}`)
+           .addField("❯ Ping", `${Math.round(bot.ping)}ms`)
             .addField("❯ CPU", `\`\`\`yaml\n${os.cpus().map(i => `${i.model}`)[0]}\`\`\``)
             .addField("❯ CPU Kullanımı", `%${percent.toFixed(2)}`)
-            .addField("❯ İşletim Sistemi", `\`\`\`${os.platform()} | ${os.arch()} Bit \`\`\``) 
+       .setFooter(bot.user.username + ' ', bot.user.avatarURL)
+      .setTimestamp()
         message.channel.send(botBilgi)
     });
 };
