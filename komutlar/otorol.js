0c8dd963c88d357d5572e0c2127d87abc0990e48
Glitch (silk-wrinkle) <none>
2019-09-16 17:22:17 +0000

    🏇💺 Checkpoint ./komutlar/otorol.js:3528245/1150

--- komutlar/otorol.js
+++ komutlar/otorol.js
@@ -2,7 +2,7 @@
 const Discord = require('discord.js')
 const db = require('quick.db');
 const ayarlar = require('../ayarlar.json')
-
+const emojiler = require('../emojiler.json');
 exports.run = async(client, message, args) => {
 
   if (!message.member.hasPermission('MANAGE_ROLES')) return message.channel.send(':no_entry: Otorol ayarlamak için `Rolleri Yönet` yetkisine sahip olman gerek.')
@@ -10,18 +10,19 @@ exports.run = async(client, message, args) => {
   
     if (args[0] == 'ayarla') {
  let rol = message.mentions.roles.first() || message.guild.roles.get(args.join(' '))
-  let 
-  let tworole;
-  if (!rol) return message.channel.send(':no_entry: Otorol ayarlamanız için bir rol etiketlemeniz gerek. `/otorol ayarla @Üye #kanal`')
-  else newRole = message.mentions.roles.first().id
-  let isim = message.mentions.roles.first().name  
-  let otorolkanal = message.mentions.channels.first();
-  if (!otorolkanal) return message.channel.send(':no_entry: Otorol ayarlamanız için bir rol etiketlemeniz gerek. `g!otorol ayarla @Üye #kanal`')
+  let yeniRol;
+  let ikinciRol;
+  if (!rol) return message.channel.send(emojiler.basarisiz + ' Yeni kişilere vereceğim rolü etiketlemelisin. Kullanım: `!otorol ayarla @Rol #kanal`')
+  else yeniRol = message.mentions.roles.first().id
+ let rolisim = message.mentions.roles.first().name
+      let isim = message.mentions.roles.first().name  
+  let kanal = message.mentions.channels.first();
+  if (!kanal) return message.channel.send(emojiler.basarisiz + ' Bilgilendirme mesajlarını atacağım yeri etiketlemelisin. Kullanım: `!otorol ayarla @Rol #kanal`')
     db.set(`otorolisim_${message.guild.id}`, isim)
   let i = await  db.set(`otorolKanal_${message.guild.id}`, message.mentions.channels.first().id)
-  let otorol = await db.set(`autoRole_${message.guild.id}`, newRole)
-  if (!message.guild.roles.get(newRole)) return message.channel.send(":no_entry: Etiketlediğiniz rol bulunamadı, etiketlediğiniz rolün etiketlenebilirliğinin aktif olduğundan emin olunuz.")
-    message.channel.send(`Otorol, <@&${newRole}> mesaj kanalı <#${i}> olarak ayarlandı.`)  
+  let otorol = await db.set(`autoRole_${message.guild.id}`, yeniRol)
+  if (!message.guild.roles.get(yeniRol)) return message.channel.send(emojiler.basarisiz+" Etiketlediğin rolü bulamadım. Rolün etiketlenebilir olduğundan emin olmalısın.")
+    message.channel.send(`${emojiler.onaylı} Yeni kişilere vereceğim rol \`${rolisim}\`\n Bilgilendirme kanalı ise <#${i}> olarak ayarlandı.`)  
      
   } 
 
