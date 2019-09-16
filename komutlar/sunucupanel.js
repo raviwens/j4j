cf138db7eaae9f1c1b3dcb52cb6ea3ea7e67707d
Glitch (silk-wrinkle) <none>
2019-09-16 19:31:42 +0000

    🏫💖 Checkpoint ./komutlar/sunucupanel.js:3528245/679

--- komutlar/sunucupanel.js
+++ komutlar/sunucupanel.js
@@ -3,11 +3,12 @@ const Discord = require('discord.js')
 const db = require('quick.db')
 const emojiler= require('../emojiler.json')
 exports.run = async (client, message, args) => {
-if (args[0] === "kapat") {
+if(args[0] < 1) return message.channel.send(emojiler.basarisiz +` Bir eylem belirtmelisiniz. \n${emojiler.gold2} Sunucu İstatistik panelini kurmak için \`!sunucupanel kur\`\n${emojiler.gold2} Sunucu İstatistik panelini temizlemek için\`!sunucupanel sıfırla\` \nyazınız. `);
+  if (args[0] === "sıfırla") {
   let k = db.fetch(`spanel_${message.guild.id}`)
   let k1 = db.fetch(`spanelUye_${message.guild.id}`)
   let k2 = db.fetch(`spanelBot_${message.guild.id}`)
-   client.category.get(k).delete()
+   client.channels.get(k).delete()
 
   client.channels.get(k1).delete()
   client.channels.get(k2).delete()
@@ -16,13 +17,13 @@ if (args[0] === "kapat") {
  await db.delete(`spanel_${message.guild.id}`)
   await db.delete(`spanelUye_${message.guild.id}`)
   await db.delete(`spanelBot_${message.guild.id}`)
-  return message.channel.send(emojiler.gold1 + " Sunucu Analiz paneli silindi.");
-}else{
+  return message.channel.send(emojiler.gold1 + " Sunucu İstatistik paneli silindi.");
+}if (args[0] ==="kur"){
   if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(`Bu komutu kullanabilmek için "\`Yönetici\`" yetkisine sahip olmalısın.`);
 var sunucupanel = message.guild.createChannel("Sunucu Panel", "category").then(sp => {
-
 db.set(`spanel_${message.guild.id}`,sp.id)
-var toplamkişi = message.guild.createChannel(`Toplam Kullanıcı: ${message.guild.members.filter(m => !m.user.bot).size}`, "voice").then(ss => {
+ client.channels.get(sp.id).setPosition(0)
+  var toplamkişi = message.guild.createChannel(`Toplam Kullanıcı: ${message.guild.members.filter(m => !m.user.bot).size}`, "voice").then(ss => {
 ss.setParent(sp)
 db.set(`spanelUye_${message.guild.id}` , ss.id)
 let role = message.guild.roles.find(a => a.name === "@Everyone");
@@ -51,7 +52,7 @@ permLevel: 3
 };
 
 exports.help = {
-name: 'panel-kur',
+name: 'sunucupanel',
 description: 'Sunucudaki üye sayısını kanallarda gösterecek bir sistem kurar.',
-usage: 'panel-kur'
+usage: 'sunucupanel'
 };
