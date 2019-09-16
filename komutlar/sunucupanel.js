1d28c48003b677e299f888b7c0b25237e707c94b
Glitch (silk-wrinkle) <none>
2019-09-16 19:08:50 +0000

    🎳🎟 Checkpoint ./komutlar/sunucupanel.js:3528245/751 ./komutlar/guvenlik.js:3528245/60

--- komutlar/sunucupanel.js
+++ komutlar/sunucupanel.js
@@ -3,11 +3,19 @@ const Discord = require('discord.js')
 const db = require('quick.db')
 const emojiler= require('../emojiler.json')
 exports.run = async (client, message, args) => {
-if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(`Bu komutu kullanabilmek için "\`Yönetici\`" yetkisine sahip olmalısın.`);
+if (args[0] === "kapat") {
+  let k = db.fetch(`spanel_${message.guild.id}`)
+  let k1 = db.fetch(`spanelUye_${message.guild.id}`)
+  let k2 = db.fetch(`spanelBot_${message.guild.id}`)
+  
+  db.delete(`spanel_${message.guild.id}`)
+  db.delete(`spanelUye_${message.guild.id}`)
+  db.delete(`spanelBot_${message.guild.id}`)
+}else{
+  if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(`Bu komutu kullanabilmek için "\`Yönetici\`" yetkisine sahip olmalısın.`);
 var sunucupanel = message.guild.createChannel("Sunucu Panel", "category").then(sp => {
 
-
-
+db.set(`spanel_${message.guild.id}`,sp.id)
 var toplamkişi = message.guild.createChannel(`Toplam Kullanıcı: ${message.guild.members.filter(m => !m.user.bot).size}`, "voice").then(ss => {
 ss.setParent(sp)
 db.set(`spanelUye_${message.guild.id}` , ss.id)
@@ -25,9 +33,10 @@ CONNECT: false,
 });
 })
 })
-message.channel.send(`${emojiler.onaylı} Sunucu İstatistik paneli kuruldu.`);
-}
 
+await message.channel.send(`${emojiler.onaylı} Sunucu İstatistik paneli kuruldu.`);
+}
+}
 exports.conf = {
 enabled: true,
 guildOnly: false,
