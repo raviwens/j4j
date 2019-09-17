3206b67c21ae3165d263a8f19174d0bb6ca56f33
Glitch (silk-wrinkle) <none>
2019-09-17 04:05:17 +0000

    🌏📇 Checkpoint ./komutlar/sunucupanel.js:3528245/1195

--- komutlar/sunucupanel.js
+++ komutlar/sunucupanel.js
@@ -6,7 +6,9 @@ exports.run = async (client, message, args) => {
   if (!message.member.hasPermission('MANAGE_GUILD')) return message.channel.send(`${emojiler.basarisiz} Bu komudu kullanabilmek için "Sunucuyu Yönet" yetkisine sahip olman gerek.`)
   if(!args[0]) return message.channel.send(emojiler.basarisiz +` Bir eylem belirtmelisiniz. \n${emojiler.gold2} Sunucu İstatistik panelini kurmak için \n${emojiler.gold1}\`!sunucupanel kur\`\n${emojiler.gold2} Sunucu İstatistik panelini temizlemek için\n${emojiler.gold1}\`!sunucupanel sıfırla\` \nyazınız. `);
   if (args[0] === "sıfırla") {
-  let k = db.fetch(`spanel_${message.guild.id}`)
+  if(!sunucupanel) return message.channel.send(emojiler.basarisiz + `Sunucu İstatistik Panelinin kurulu olduğuna emin misiniz? \n${emojiler.gold2} Sıfırlamak için\n${emojiler.gold1}\`!sunucupanel sıfırla\`\n yazınız.`);
+
+    let k = db.fetch(`spanel_${message.guild.id}`)
   let k1 = db.fetch(`spanelUye_${message.guild.id}`)
   let k2 = db.fetch(`spanelBot_${message.guild.id}`)
  await  client.channels.get(k).delete()
@@ -22,11 +24,11 @@ await db.delete(`spanelDurum_${message.guild.id}`)
  
     return message.channel.send(emojiler.gold1 + " Sunucu İstatistik paneli silindi.");
 }if (args[0] ==="kur"){
+db.set(`spanelDurum_${message.guild.id}`, 'acik')
  let durum = db.fetch(`spanelDurum${message.guild.id}`)
-  if(durum === "acik") return message.channel.send(emojiler.basarisiz + `Sunucu İstatistik Paneli zaten kurulu. \n${emojiler.gold2} Sıfırlamak için\n${emojiler.gold1}\`!sunucupanel sıfırla\`\n yazınız.`);
+  if(!sunucupanel) return message.channel.send(emojiler.basarisiz + `Sunucu İstatistik Paneli zaten kurulu. \n${emojiler.gold2} Sıfırlamak için\n${emojiler.gold1}\`!sunucupanel sıfırla\`\n yazınız.`);
 var sunucupanel = message.guild.createChannel("Sunucu Panel", "category").then(sp => {
 db.set(`spanel_${message.guild.id}`,sp.id)
-db.set(`spanelDurum_${message.guild.id}`, 'acik')
  
   client.channels.get(sp.id).setPosition(0)
  
