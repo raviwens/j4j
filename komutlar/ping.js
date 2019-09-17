198c611ed5b7b10f38dd51ceef346ee77fb1f5f8
Glitch (silk-wrinkle) <none>
2019-09-17 15:32:22 +0000

    🍽😻 Checkpoint ./komutlar/yaz.js:3528245/51 ./komutlar/istatistik.js:3528245/354 ./komutlar/ping.js:3528245/538

--- komutlar/ping.js
+++ komutlar/ping.js
@@ -1,9 +1,11 @@
 const Discord = require('discord.js');
 const db = require('quick.db');
 const emojiler = require('../emojiler.json');
+const moment = require('moment');
+require('moment-duration-format');
 exports.run = (client, message, args) => {
-
-  message.channel.send(`${emojiler.gold1} **Tepki Gecikmesi** \`${message.createdTimestamp - message.createdTimestamp}\`**ms**\n${emojiler.gold1} **Bot Gecikmesi** \`${Math.round(client.ping)}\`**ms**`);
+       
+  message.channel.send(`${emojiler.gold1} **Tepki Gecikmesi** \`${Date.now() - message.createdTimestap}\`**ms**\n${emojiler.gold1} **Bot Gecikmesi** \`${Math.round(client.ping)}\`**ms**`);
 }
 
 exports.conf = {
