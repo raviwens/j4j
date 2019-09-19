0ca83d7c771bf3f3642e4c0f8f16459e80f110dd
Glitch (exuberant-cactus) <none>
2019-09-19 20:33:44 +0000

    🍭🚗 Checkpoint ./emojiler.json:3528245/42 ./komutlar/ayarlar.js:3528245/1722

--- komutlar/ayarlar.js
+++ komutlar/ayarlar.js
@@ -15,17 +15,21 @@ exports.run = async (client, message, args) => {
 let botkoruma = db.fetch(`botkoruma_${message.guild.id}`)
 
   const sayfa = [`
-${db.has(`botkoruma_${message.guild.id}`) ? `**Bot Koruma:** ${client.channels.get(db.fetch(`botkoruma_${message.guild.id}`))} `: `${emojiler.gold2} **Bot koruma kanalını ayarlamak için:** \n\`!botkoruma #kanal\``}
+\n${db.has(`botkoruma_${message.guild.id}`) ? `**Bot Koruma:** ${client.channels.get(db.fetch(`botkoruma_${message.guild.id}`))} `: `${emojiler.gold2} **Bot koruma kanalını ayarlamak için:** \n${emojiler.kup} \`!botkoruma #kanal\``}
+\n${db.has(`yazilihgbb_${message.guild.id}`) ? `**Yazılı HGBB:** ${client.channels.get(db.fetch(`yazilihgbb_${message.guild.id}`))} `: `${emojiler.gold2} **Yazılı HGBB kanalını ayarlamak için:** \n${emojiler.kup} \`!yazılı-hgbb #kanal\``}
+\n${db.has(`botkoruma_${message.guild.id}`) ? `**Resimli HGBB:** ${client.channels.get(db.fetch(`resimlihgbb_${message.guild.id}`))} `: `${emojiler.gold2} **Resimli HGBB kanalını ayarlamak için:** \n${emojiler.kup} \`!resimli-hgbb #kanal\``}
+\n${db.has(`botkoruma_${message.guild.id}`) ? `**Bot Koruma:** ${client.channels.get(db.fetch(`botkoruma_${message.guild.id}`))} `: `${emojiler.gold2} **Bot koruma kanalını ayarlamak için:** \n${emojiler.kup} \`!botkoruma #kanal\``}
+\n${db.has(`botkoruma_${message.guild.id}`) ? `**Bot Koruma:** ${client.channels.get(db.fetch(`botkoruma_${message.guild.id}`))} `: `${emojiler.gold2} **Bot koruma kanalını ayarlamak için:** \n${emojiler.kup} \`!botkoruma #kanal\``}
 
   
 `]
   
   const ayarlar = new Discord.RichEmbed()
-  .setColor(0x36393E)
+  .setColor("0x70ff96")
   .setTitle(`${emojiler.gold1} **${message.guild.name} Ayarlar**`)
   .setDescription(sayfa)
   .setTimestamp()
-  
+  .setFooter(`${client.user.username} Ayarlar`)
   message.channel.send(ayarlar)
 
  
