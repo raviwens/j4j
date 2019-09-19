bdda05a9d66b852da9c6fcaf515ffd97d174814a
Glitch (exuberant-cactus) <none>
2019-09-19 20:09:18 +0000

    🍄🍉 Checkpoint ./komutlar/ayarlar.js:3528245/1891

--- komutlar/ayarlar.js
+++ komutlar/ayarlar.js
@@ -14,26 +14,18 @@ exports.run = async (client, message, args) => {
 
 let botkoruma = db.fetch(`botkoruma_${message.guild.id}`)
 
-  const sayfa = [`**${message.guild.name} | Ayarları**
-\n**Sunucu prefixi :** /
-\n**Bot Koruma:** ${db.has(`botkoruma_${message.guild.id}`) ? `Kapalı` : `d`}
-\n**Küfür engeli:** ${db.has(`kufur_${message.guild.id}`) ? `Açık ` : ` Kapalı ` }
-\n**Büyük harf engeli:** ${db.has(`capsEngel_${message.guild.id}`) ? `Açık ` : ` Ayarlanmamış ` }
-\n**Otorol:** ${db.has(`autoRole_${message.guild.id}`) ? ` \`@${message.guild.roles.get(db.fetch(`autoRole_${message.guild.id}`)).name}\`` : ` Ayarlanmamış `}
-\n**Otorol Kayıt Kanalı:** ${db.has(`otorolKanal_${message.guild.id}`) ? ` \`${message.guild.channels.get(db.fetch(`otorolKanal_${message.guild.id}`)).name}\` ` : ` Ayarlanmamış `}
-\n**Sayaç kanalı:** ${db.has(`sayacKanal_${message.guild.id}`) ? ` \`${message.guild.channels.get(db.fetch(`sayacKanal_${message.guild.id}`)).name}\` `: ` Ayarlanmamış  `}
-\n**Sayaç:** ${db.has(`sayacSayi_${message.guild.id}`) ?` \`${db.fetch(`sayacSayi_${message.guild.id}`)}\` ` : `Ayarlanmamış `}
-\n**Giriş Çıkış kanalı:** ${db.has(`hgKanal_${message.guild.id}`) ? ` ${client.channels.get(db.fetch(`hgKanal_${message.guild.id}`))} `  : `Ayarlanmamış `}
+  const sayfa = [`
+${db.has(`botkoruma_${message.guild.id}`) ? `**Bot Koruma:** ${client.channels.get(db.fetch(`botkoruma_${message.guild.id}`))} `: `**Bot koruma kanalını ayarlamak için:** \n\`!botkoruma #kanal\``}
 
   
 `]
   
-  const ayarReis = new Discord.RichEmbed()
+  const ayarlar = new Discord.RichEmbed()
   .setColor(0x36393E)
   .setDescription(sayfa)
   .setTimestamp()
   
-  message.channel.send(ayarReis)
+  message.channel.send(ayarlar)
 
  
 };
