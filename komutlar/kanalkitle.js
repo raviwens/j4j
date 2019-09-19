94fe9c1c9752ebf0173c14a36c4842edd249b475
Glitch (exuberant-cactus) <none>
2019-09-19 18:33:24 +0000

    👜🐄 Checkpoint ./komutlar/kanalkitle.js:3528245/53

--- komutlar/kanalkitle.js
+++ komutlar/kanalkitle.js
@@ -3,18 +3,18 @@ const db = require('quick.db');
 const emojiler = require('../emojiler.json');
 exports.run = async (client, message, args) => {
  let herkes = message.guild.roles.find(a => a.name === "@everyone");
-if (args[0] === "kilit kapat") {
+if (args[0] === "unlock") {
     
    message.channel.send(`${emojiler.onaylı} ${message.channel} başarıyla açıldı.`);
   
     
   }
-  if(args[0] === "kilit aç"){
+  if(args[0] === "lock"){
   message.channel.overwritePermissions(herkes, {
         'SEND_MESSAGES': false,
 
 });
-message.channel.send(emojiler.onaylı + ` ${message.channel} başarıyla kitlendi. \n${emojiler.gold2} Kanalı açmak için:\n${emojiler.gold1} \`!kilit aç\``);
+message.channel.send(emojiler.onaylı + ` ${message.channel} başarıyla kitlendi. \n${emojiler.gold2} Kanalı açmak için:\n${emojiler.gold1} \`!kanal unlock\``);
   }}
 
 exports.conf = {
