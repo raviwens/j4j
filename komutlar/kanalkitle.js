8666a45dd3278d54af79ab17256eeb98378fcb16
Glitch (exuberant-cactus) <none>
2019-09-19 18:21:25 +0000

    💐🐾 Checkpoint ./komutlar/kanalkitle.js:3528245/1516 ./komutlar/guvenlik.js:3528245/718

--- komutlar/kanalkitle.js
+++ komutlar/kanalkitle.js
@@ -2,15 +2,20 @@ const Discord = require('discord.js');
 const db = require('quick.db');
 const emojiler = require('../emojiler.json');
 exports.run = async (client, message, args) => {
-  if(message.member.hasPermission("ADMINSTOR")) return message.channel.send(`${emojiler.basarisiz} Bu işlemi yapmak için \`ADMINSTOR\` yetkisine sahip olmanız gerekmektedir.`);
  let herkes = message.guild.roles.find(a => a.name === "@everyone");
- if(herkes = 
+if (args[0] === "kilit kapat") {
+    
+   message.channel.send(`${emojiler.onaylı} ${message.channel} başarıyla açıldı.`);
+  
+    
+  }
+  if(args[0] === "kilit aç"){
   message.channel.overwritePermissions(herkes, {
         'SEND_MESSAGES': false,
 
-})
-message.channel.send(emojiler.onaylı + ` ${message.channel} başarıyla kitlendi. \n${emojiler.gold2} Kanalı açmak için:\n${emojiler.gold1} \`!kilit aç\``)
-}
+});
+message.channel.send(emojiler.onaylı + ` ${message.channel} başarıyla kitlendi. \n${emojiler.gold2} Kanalı açmak için:\n${emojiler.gold1} \`!kilit aç\``);
+  }}
 
 exports.conf = {
   enabled: true,
@@ -20,7 +25,7 @@ exports.conf = {
 };
 
 exports.help = {
-  name: 'kitle',
+  name: 'kanal',
   description: '',
   usage: ''
 };
