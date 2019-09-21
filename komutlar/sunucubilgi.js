f1aecd78040b46ef079c4a4e2dc8ac32f79af375
Glitch (exuberant-cactus) <none>
2019-09-21 14:45:06 +0000

    💧👮 Checkpoint ./komutlar/sunucubilgi.js:3528245/766

--- komutlar/sunucubilgi.js
+++ komutlar/sunucubilgi.js
@@ -4,18 +4,24 @@ const ayarlar = require('../ayarlar.json');
 const moment = require('moment')
 const emojiler = require("../emojiler.json");
 exports.run = (bot, message, params) => {
-  let afk = moment.duration(message.guild.afkTimeout)
+ let bolge = message.guild.region
+ .replace("eu-","🇪🇺 Avrupa (Merkez)")
+ .replace("singapore","🇸🇬 Singapur")
+ .replace("india", "🇮🇳 Hindistan")
+ .replace("us-central","🇺🇸 Amerika (Merkez)")
+ .replace("london","🇬🇧 Londra")
+ 
   const embed = new Discord.RichEmbed()
    .setColor("42bcf5")
    .setAuthor(message.guild.name, message.guild.userURL)
    .setThumbnail(message.guild.iconURL)
-   .addField('Sunucu ID:', message.guild.id, true)
-   .addField('Sunucu Bölgesi:', message.guild.region, true)
-   .addField('Sunucu Sahibi:', message.guild.owner, true)
+  .addField('Sunucu Sahibi:', message.guild.owner, true)
+    .addField('Sunucu ID:', message.guild.id, true)
+   .addField('Sunucu Bölgesi:', bolge , true)
    .addField('Doğrulama seviyesi:', message.guild.verificationLevel, true)
-   .addField('Üyeler:', `${emojiler.gold1} \`${message.guild.members.filter( member => member.user.bot).size}\` bot \n${emojiler.gold1\`${message.guild.memberCount}\` üye`, true)
+   .addField('Üyeler [' + message.guild.members.size+"]:", `${emojiler.gold1} \`${message.guild.members.filter( member => member.user.bot).size}\` bot \n${emojiler.gold1} \`${message.guild.memberCount}\` üye`, true)
    .addField('Roller:', message.guild.roles.map(role => role.name).join(' - '), true)
-   .addField('Kanallar:', emojiler.gold2 +` \`${message.guild.channels.filter(chan => chan.type === 'voice').size}\` sesli/ \` ${message.guild.channels.filter(chan => chan.type === 'text').size}\` metin / \`${message.guild.channels.size}\` toplam`, true)
+   .addField('Kanallar ['+ message.guild.channels.size+"]:", emojiler.gold2 +` \`${message.guild.channels.filter(chan => chan.type === 'voice').size}\` sesli/ \` ${message.guild.channels.filter(chan => chan.type === 'text').size}\` metin / \`${message.guild.channels.filter(chan => chan.type === "category").size}\` kategori`, true)
    .setFooter('Server Guard', message.guild.iconURL)
    .setTimestamp()
    message.channel.send({embed});
