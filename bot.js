da36d9dc0fb9a8c12c6e4cca3db10998f45e2ad2
Glitch (exuberant-cactus) <none>
2019-09-21 11:48:17 +0000

    📼🌏 Checkpoint ./bot.js:3528245/16

--- bot.js
+++ bot.js
@@ -480,7 +480,7 @@ let kanal= db.fetch(`tagaKanal_${message.guild.id}`);
 
     if(message.author.username.includes(tag)) {
       message.member.addRole(rol)
-      client.channels.get(kanal).send(emojiler.gold1 +`\`${message.author.id}\` adlı kullanıcı \`${tag}\`'ni ismine eklediği için rol aldı.`);
+      client.channels.get(kanal).send(emojiler.gold1 +`\`${message.author.username}\` adlı kullanıcı \`${tag}\`'ni ismine eklediği için rol aldı.`);
     }else{
       message.member.removeRole(rol)
     
