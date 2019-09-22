fa6a3956813e742a9cc03deae11f1dca78b81d3b
Glitch (exuberant-cactus) <none>
2019-09-22 06:00:09 +0000

    👷📺 Checkpoint ./util/eventLoader.js:3528245/250 ./bot.js:3528245/1546 ./events/giriş.js:3528245/1826 ./events/ready.js:3528245/78

--- bot.js
+++ bot.js
@@ -316,52 +316,7 @@ let mkanal = client.channels.get(db.fetch(`yazilihgbb_${member.guild.id}`))
 mkanal.send(`${emojiler.olamaz} **${member.user.username}** suncudan ayrıldı, puffff.`);
 });
 //GIRIS CIKIS
-client.on('guildMemberAdd', async member => {
-  let rkanal = client.channels.get(db.fetch(`resimlihgbb_${member.guild.id}`))
-  if(member.user.bot === true) return;
-  const Canvas= require('canvas')
-    , Image = Canvas.Image
-    , Font = Canvas.Font
-    , path = require('path');
-
-  const canvas = Canvas.createCanvas(360, 250)
-
-  const ctx = canvas.getContext('2d');
- 
-  const background = await Canvas.loadImage('https://cdn.discordapp.com/attachments/623947123473514496/623947203144056842/sunucuyakatildi.jpg');
-  ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-  
-  ctx.strokeStyle = '#74037b';
-	ctx.strokeRect(0, 0, canvas.width, canvas.height);
-  
-  ctx.fillStyle = `#BAF79C`;
-	
-	ctx.textAlign = "center";
-  var kontrol;
- if(member.user.username.length < 14) kontrol =ctx.font = `33px "Sans Serif"`;
-  if(member.user.username.length > 14) kontrol= ctx.font = `18px "Sans Serif"`;
-   ctx.fillText(`${member.user.username.toUpperCase()}`, 180, 210);
-ctx.fillStyle = `#12fccd`;
- ctx.font = `16px "Sans Serif"`;
-   ctx.fillText(`${client.user.username}`, 78, 235);
- 
-  let avatarURL = member.user.avatarURL || member.user.defaultAvatarURL
-  const { body } = await request.get(avatarURL);
-	const avatar = await Canvas.loadImage(body);
-  
-  ctx.beginPath();
-	ctx.lineWidth = 4;
-  ctx.fill()
-	ctx.lineWidth = 4;
-	ctx.arc(112 + 55, 55 + 55, 55, 0, 2 * Math.PI, false);
-	ctx.clip();
-	ctx.drawImage(avatar, 112, 55, 270, 170);
-  
-  const dosya= new Discord.Attachment(canvas.toBuffer(), 'SERVER-GUARD.png');
-  rkanal.send(dosya)
-
-  
-});client.on('guildMemberRemove', async member => {
+client.on('guildMemberRemove', async member => {
   let rkanal = client.channels.get(db.fetch(`resimlihgbb_${member.guild.id}`))
   if(member.user.bot === true) return;
   const Canvas = require('canvas')
