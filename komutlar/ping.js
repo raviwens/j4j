9d575cdc0dae2f2de94b5210fcfa4b5cd97640d0
Glitch (exuberant-cactus) <none>
2019-09-22 08:11:11 +0000

    ☃️👀 Checkpoint ./komutlar/ping.js:3528245/1953 ./komutlar/rolal.js:3528245/1858

--- komutlar/ping.js
+++ komutlar/ping.js
@@ -7,7 +7,9 @@ exports.run = async (client, message, args) => {
    var olcum = await message.channel.send(emojiler.bekliyor + ' Ölçüm yapılıyor, lütfen bekleyiniz...');
  var sonuc = await message.channel.send(emojiler.gold2 + " Veriler alındı...").then(msg => msg.delete(3000))
      await olcum.edit( `${emojiler.gold1} **Tepki Gecikmesi** \`${Math.round((sonuc.createdTimestamp - olcum.createdTimestamp - client.ping) )}\`**ms**\n${emojiler.gold1} **Bot Gecikmesi** \`${Math.round(client.ping)}\`**ms**`);
-
+///
+  
+  
 }
 
 exports.conf = {
