14f744397c630b5fbd4240146797f21ab89b413a
Glitch (exuberant-cactus) <none>
2019-09-24 19:56:03 +0000

    ☺️🌽 Checkpoint ./komutlar/rolrengi.js:3528245/1981

--- komutlar/rolrengi.js
+++ komutlar/rolrengi.js
@@ -1,13 +1,21 @@
 const Discord = require('discord.js')
 const ayarlar = require('../ayarlar.json')
+const emojiler = require('../emojiler.json')
+
 exports.run = (client, message, args) => {
   let prefix = ayarlar.prefix
   let rol = args[0]
       let renk = args[1]
-   if (!renk) return message.channel.sendEmbed(new Discord.RichEmbed().setDescription(`Bir Renk Kodu belirtmelisin\n Örnek Kullanım ${prefix}rolrengi @rol rolkodu\n Renk Kodunun başına # Koymayın`));
+      if(args[1]> 18) return  message.channel.send(emojiler.basarisiz + " Lütfen doğru bir rol ID giriniz.");
+     if(args[1] < 18) return  message.channel.send(emojiler.basarisiz + " Lütfen doğru bir rol ID giriniz.");
+     
+    if (!renk) return message.channel.sendEmbed(new Discord.RichEmbed().setDescription(`Bir Renk Kodu belirtmelisin\n Örnek Kullanım \`!rolrengi rolid hexcode\`\nNot: **hexcode**'ün başına \`#\` koymayınız.`));
    message.guild.roles.get(rol).setColor(renk)
-  message.channel.send(`Başarıyla rol rengi #${renk} olarak ayarlandı`);
-}
+  message.channel.send(emojiler.onaylı +` **${rol}** ID'li rolün rengi \`#${renk}\` olarak ayarlandı.`);
+    
+          
+      
+ }
 
 exports.conf = {
     enabled: true,
