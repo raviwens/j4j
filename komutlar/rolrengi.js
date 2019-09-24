202194309b95bd1977e3b760eb1fd3db2a0c55d6
Glitch (exuberant-cactus) <none>
2019-09-24 20:07:05 +0000

    ⛲️🚖 Checkpoint ./komutlar/rolrengi.js:3528245/380

--- komutlar/rolrengi.js
+++ komutlar/rolrengi.js
@@ -6,12 +6,10 @@ exports.run = (client, message, args) => {
   let prefix = ayarlar.prefix
   let rol = args[0]
       let renk = args[1]
-      if(args[1]> 18) return  message.channel.send(emojiler.basarisiz + " Lütfen doğru bir rol ID giriniz.");
-     if(args[1] < 18) return  message.channel.send(emojiler.basarisiz + " Lütfen doğru bir rol ID giriniz.");
      
-    if (!renk) return message.channel.sendEmbed(new Discord.RichEmbed().setDescription(`Bir Renk Kodu belirtmelisin\n Örnek Kullanım \`!rolrengi rolid hexcode\`\nNot: **hexcode**'ün başına \`#\` koymayınız.`));
-   message.guild.roles.get(rol).setColor(renk)
-  message.channel.send(emojiler.onaylı +` **${rol}** ID'li rolün rengi \`#${renk}\` olarak ayarlandı.`);
+    if (!renk) return message.channel.send(`Bir Renk Kodu belirtmelisin\n Örnek Kullanım \`!rolrengi rolid hexcode\`\nNot: **hexcode**'ün başına \`#\` koymayınız.`);
+  if(args[1] === 18)return message.guild.roles.get(rol).setColor(renk)
+ if(args[1] === 18) return message.channel.send(emojiler.onaylı +` **${rol}** ID'li rolün rengi \`#${renk}\` olarak ayarlandı.`);
     
           
       
@@ -21,7 +19,7 @@ exports.conf = {
     enabled: true,
     guildOnly: false,
     aliases: [],
-    permLevel: 4,
+    permLevel: 5,
     kategori: 'bot'
 };
 
