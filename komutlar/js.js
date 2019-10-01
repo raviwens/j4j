a410ede20db3944901b829ca41aeb96fb1fdae9a
Glitch (celestial-night) <none>
2019-10-01 18:44:07 +0000

    🎽👐 Checkpoint ./bot.js:3528245/508 ./komutlar/js.js:3528245/369

--- komutlar/js.js
+++ komutlar/js.js
@@ -4,19 +4,8 @@ const ayarlar = require('../ayarlar.json');
 const db = require("quick.db")
 
 exports.run = (client, message, args) => {
-message.delete(2000);
-  message.delete()
- if (message.channel.id==="626482511710781453"){
-  let alrol = message.guild.roles.get("626484310169354251");
-   message.member.addRole(alrol);  
-    
-   message.reply("JS rolünü aldın.").then(msg => msg.delete(10000))
- }else{
-  message.channel.send("Bu kodu yanlızca **bot komut** kanalında kullanabilirsin.");
- }
-  }
 
-
+}
 
 exports.conf = {
   enabled: true,
@@ -26,7 +15,7 @@ exports.conf = {
 };
 
 exports.help = {
-  name: 'js',
+  name: 'ping',
   description: 'Tüm komutları gösterir.',
   usage: 'yardım [komut]'
 };
