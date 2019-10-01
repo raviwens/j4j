3569e9d8425d93cbec77185260094e2427b6ad1b
Glitch (exuberant-cactus) <none>
2019-10-01 18:20:07 +0000

    📟🍙 Checkpoint ./util/eventLoader.js:3528245/101 ./util/Util.js:3528245/3518 ./bot.js:3528245/1129

--- events/message.js
+++ events/message.js
@@ -1,23 +1,21 @@
-const ayarlar = require('../ayarlar.json');
+const ayarlar = require("../ayarlar.json");
 let talkedRecently = new Set();
 module.exports = message => {
   if (talkedRecently.has(message.author.id)) {
     return;
   }
   talkedRecently.add(message.author.id);
-    setTimeout(() => {
+  setTimeout(() => {
     talkedRecently.delete(message.author.id);
   }, 2500);
   let client = message.client;
   if (message.author.bot) return;
   if (!message.content.startsWith(ayarlar.prefix)) return;
-  let command = message.content.split(' ')[0].slice(ayarlar.prefix.length);
-  let params = message.content.split(' ').slice(1);
+  let command = message.content.split(" ")[0].slice(ayarlar.prefix.length);
+  let params = message.content.split(" ").slice(1);
   let perms = client.elevation(message);
   let probs;
-  
 
-  
   if (client.commands.has(command)) {
     probs = client.commands.get(command);
   } else if (client.aliases.has(command)) {
@@ -27,5 +25,4 @@ module.exports = message => {
     if (perms < probs.conf.permLevel) return;
     probs.run(client, message, params, perms);
   }
-
 };
