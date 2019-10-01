c0fd6cc5d3bbb9b535f751380c2718e211eda65b
Glitch (celestial-night) <none>
2019-10-01 18:33:06 +0000

    👳🎏 Checkpoint ./bot.js:3528245/10362 ./ayarlar.json:3528245/273 ./events/ready.js:3528245/263 ./events/message.js:3528245/1802

--- events/ready.js
+++ events/ready.js
@@ -6,11 +6,8 @@ const ayarlar = require('../ayarlar.json');
 var prefix = ayarlar.prefix;
 
 module.exports = client => {
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
-  client.user.setStatus("online");
-  
-
-      
-        client.user.setActivity("Sunucu İstatistik Hizmetleri");
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] Ping ~ 》 Gerekli kurulum tamamlandı!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] Ping ~ 》 ${client.user.tag} olarak giriş sağlandı...`);
+  client.user.setStatus("dnd");
+        client.user.setActivity("PingWasHere");
 };
