033e6960cb9c8ce6e3eb2a12dbb5b7dd27ed2a18
Glitch (celestial-night) <none>
2019-10-11 18:01:26 +0000

    👹⛹ Checkpoint ./events/message.js:3528245/108 ./util/eventLoader.js:3528245/384

--- events/message.js
+++ events/message.js
@@ -10,15 +10,15 @@ module.exports = message => {
   let client = message.client;
   if (message.author.bot) return;
   if (!message.content.startsWith(client.ayarlar.prefix)) return;
-  let command = message.content.split(" ")[0].slice(client.ayarlar.prefix.length);
+  let komut = message.content.split(" ")[0].slice(client.ayarlar.prefix.length);
   let params = message.content.split(" ").slice(1);
   let perms = client.elevation(message);
   let probs;
 
-  if (client.commands.has(command)) {
-    probs = client.commands.get(command);
-  } else if (client.aliases.has(command)) {
-    probs = client.commands.get(client.aliases.get(command));
+  if (client.commands.has(komut)) {
+    probs = client.commands.get(komut);
+  } else if (client.aliases.has(komut)) {
+    probs = client.commands.get(client.aliases.get(komut));
   }
   if (probs) {
     if (perms < probs.conf.permLevel) return;
