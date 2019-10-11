535989c9a019bf8589f4d2cd381d2ec08c1e9a21
Glitch (celestial-night) <none>
2019-10-11 16:41:04 +0000

    ⛄️🕵 Checkpoint ./shard.js:3528245/1190 ./events/ready.js:3528245/232

--- events/ready.js
+++ events/ready.js
@@ -7,4 +7,6 @@ module.exports = client => {
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] Ping ~ 》 ${client.user.tag} olarak giriş sağlandı...`);
   client.user.setStatus("dnd");
         client.user.setActivity(client.ayarlar.oynuyor + " PingWasHere");
+
+console.log(`[PING] Kurulum #${client.shard.id}: (${+client.shard.id+1}/${client.shard.count}) hazır.`);
 };
--- shard.js
+++ shard.js
@@ -1,13 +1,11 @@
-
-const { ShardingManager } = require('discord.js');
-const shard = new ShardingManager('./pingBot.js', {
+const { ShardingManager } = require("discord.js");
+const shard = new ShardingManager("./pingBot.js", {
   token: "NjMwMzE4Mjg1OTE2ODY0NTEy.XZmj0Q.9pEGtFJ4n_WpOVnJCFYv6GmeXOM",
-  autoSpawn: 1,
-  totalShards: 1
+  autoSpawn: true
 });
 shard.spawn(2);
-shard.on('shardCreate', (shard) => {
-    console.log(`Successfully launched shard #${shard.id} (${+shard.id+1}/${shard.manager.totalShards})`);
-});
 
-shard.on('launch', shard => console.log(`[SHARD] Shard ${shard.id}/${shard.manager.totalShards}`));
+
+shard.on("launch", shard =>
+  console.log(`[PING] Shard Başladı: ${shard.id}/${shard.manager.totalShards}`)
+);
